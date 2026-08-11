6cfaf91630639b7c5447736c3fe9a33834bb686d
Zubairilyas1 <zubairahmadsiyalkot@gmail.com>
2026-08-11 06:45:07 -0700

    Update backend/run_ingest.py

--- backend/run_ingest.py
+++ backend/run_ingest.py
@@ -6,6 +6,11 @@ from app.services.ingestor import DataIngestor
 
 print("Initializing Ingestor...")
 ingestor = DataIngestor()
-print("Scanning and Indexing vehicle statutes...")
-results = ingestor.scan_and_index(category="vehicle_statutes")
-print("Results:", results)
+
+print("Scanning and Indexing case_law...")
+results_cl = ingestor.scan_and_index(category="case_law")
+print("Results:", results_cl)
+
+print("Scanning and Indexing statutes...")
+results_st = ingestor.scan_and_index(category="statutes")
+print("Results:", results_st)
